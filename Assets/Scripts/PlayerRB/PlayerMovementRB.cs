93ac86b373682d686b5984b1a8de9f0688f94381
Ioculatorr <118347890+Ioculatorr@users.noreply.github.com>
2024-03-06 01:34:50 +0100

    CameraLock, nowy wariant movementu,
    Sporo pozytywnych zmian, poprawa kamery, zmiana movementu na bardziej nadawający się do naszego projektu.
    
    Zablokowanie kamery dopóki nie skończy się animacja podnoszenia głowy. (Chciałem zrobić też otwieranie oczu ale nie da się animować w łatwy sposób postprocessów).
    
    Filtr ASCII nadal nie działa, odkładam ten cel na dalsze konsultacje.

--- Assets/Scripts/PlayerRB/PlayerMovementRB.cs
+++ Assets/Scripts/PlayerRB/PlayerMovementRB.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementNew : MonoBehaviour
+public class PlayerMovementRB : MonoBehaviour
 {
     [Header("Movement")]
     
