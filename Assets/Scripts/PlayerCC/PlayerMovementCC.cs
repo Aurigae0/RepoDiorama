1a5ac66581bbe58b0d6d1d83d92c877b5326a783
Maciej Jędrzejczak <mjedrzejczak@edu.cdv.pl>
2024-03-06 17:47:01 +0100

    Skyboxy, Shader dynamiczny, Headbobbing, Sequance spawnu
    Dodany został sequance spawnu gracza, wraz z podnoszeniem głowy jest teraz postprocess otwierania oczu. Walczyłem z tym długo ale ostatecznie pośrednio się udało.
    
    Do tego na kamerze dodałem system impulsów które przy otrzymaniu imputu ruszają kamerą góra dół czyli headbobbing. Możemy to potem zmodyfikować, stworzyłem go na korutynie żeby niewykorzystywał Update.
    
    Możemy go też użyć pod przypięcie footstepsów.
    
    Pobawiłem się troche skyboxami, będzie ich kilka ale chciałem trochę zmienić klimat sceny. Bawiłem się też shaderami, dzięki temu będziemy mogli robić animowane, błyskające lub ruszające się materiały (a może nawet modele).
    
    Na swoim kompie mam dziwne ścinki które pojawiły się z dnia na dzień i nie umiem znaleźć na nie rozwiązania. Na buildzie ich nie ma, na twoim komputerze też nie powinno ich być.

--- Assets/Scripts/PlayerCC/PlayerMovementCC.cs
+++ Assets/Scripts/PlayerCC/PlayerMovementCC.cs
@@ -1,19 +1,28 @@
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerMovementCC : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private CinemachineImpulseSource impulseSource;
 
-    [Header("Movement")] [SerializeField] private float speed = 12f;
+
+    [Header("Movement")] 
+    
+    [SerializeField] private float speed = 12f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float dampLength;
+    
     private float damp;
     [SerializeField] private AnimationCurve dampCurve;
     private Vector3 previousDirection;
 
 
 
-    [Header("Jumping")] [SerializeField] private float jumpHeight = 5f;
+    [Header("Jumping")] 
+    
+    [SerializeField] private float jumpHeight = 5f;
     [SerializeField] private float jumpCooldown = 1.0f;
     
 
@@ -32,6 +41,26 @@ public class PlayerMovementCC : MonoBehaviour
     public bool isGrounded;
     
     //bool canDie = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FootstepsSound());
+    }
+    
+
+    IEnumerator FootstepsSound()
+    {
+        while (true)
+        {
+            // Check if the player is moving and grounded to play footsteps sound
+            if (isGrounded && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+            {
+                impulseSource.GenerateImpulse();
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
 
     
     // Update is called once per frame
